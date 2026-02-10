05fb92d4edf822acb0f9f93f95b45abb8da3ecdf
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-02-10 19:05:39 +0200

    feat: add view logic with dummy data

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -8,9 +8,13 @@ import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.service.IRegionService;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.TeacherInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,7 +24,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -29,6 +36,7 @@ public class TeacherController {
 
     private final ITeacherService teacherService;
     private final IRegionService regionService;
+    private final TeacherInsertValidator teacherInsertValidator;
 
     @GetMapping("/insert")
     public String getTeacherForm(Model model) {
@@ -41,6 +49,8 @@ public class TeacherController {
     public String teacherInsert(@Valid @ModelAttribute("teacherInsertDTO")TeacherInsertDTO teacherInsertDTO,
                                 BindingResult bindingResult, Model model,
                                 RedirectAttributes redirectAttributes) {
+
+        teacherInsertValidator.validate(teacherInsertDTO, bindingResult);
 
         if (bindingResult.hasErrors()) {
             // model.addAttribute("regionsReadOnlyDTO", regions());
@@ -60,6 +70,25 @@ public class TeacherController {
             model.addAttribute("errorMessage", e.getMessage());
             return "teacher-insert";
         }
+    }
+
+    @GetMapping({"", "/"})
+    public  String getPaginatedTeachers(@PageableDefault(page = 0, size = 5, sort = "lastname") Pageable pageable,
+                                        Model model) {
+        Page<TeacherReadOnlyDTO> teachersPage = new PageImpl<>(Stream.of(
+                        new TeacherReadOnlyDTO("ab123", "Pavlos", "Pavlopoulos", "1234", "Athens"),
+                        new TeacherReadOnlyDTO("ab124", "Nikos", "Charos", "1234", "Athens"),
+                        new TeacherReadOnlyDTO("ab125", "Kostas", "Lazaris", "1234", "Athens"),
+                        new TeacherReadOnlyDTO("ab126", "George", "petrou", "1234", "Athens"),
+                        new TeacherReadOnlyDTO("ab127", "Lydia", "Spiropoulou", "1234", "Athens"))
+                .sorted(Comparator.comparing(TeacherReadOnlyDTO::lastname))
+                .skip(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .toList(), pageable, 5
+        );
+        model.addAttribute("teachers", teachersPage.getContent());
+        model.addAttribute("page", teachersPage);
+        return "teachers-list";
     }
 
     @GetMapping("/success")
