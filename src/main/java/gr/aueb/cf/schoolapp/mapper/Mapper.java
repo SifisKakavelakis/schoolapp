27e431cd81ab11872024bc20472c2a4a59c8021f
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-02-17 16:19:20 +0200

    feat: add teacher edit dto

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -12,20 +12,20 @@ import org.springframework.stereotype.Component;
 public class Mapper {
 
     public Teacher mapToTeacherEntity(TeacherInsertDTO teacherInsertDTO) {
-        return new Teacher(null, null,teacherInsertDTO.vat(), teacherInsertDTO.firstname(), teacherInsertDTO.lastname(), null);
+        return new Teacher(null, null, teacherInsertDTO.vat(), teacherInsertDTO.firstname(), teacherInsertDTO.lastname(), null);
     }
 
-    public TeacherReadOnlyDTO mapToTeacherReadOnlyDto(Teacher teacher) {
+    public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
+        return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
+                teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
+    }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
         return new TeacherReadOnlyDTO(teacher.getUuid().toString(), teacher.getFirstname(), teacher.getLastname(),
                 teacher.getVat(), teacher.getRegion().getName());
     }
 
     public RegionReadOnlyDTO mapToRegionReadOnlyDTO(Region region) {
         return new RegionReadOnlyDTO(region.getId(), region.getName());
-    }
-
-    public TeacherEditDTO mapToTeacherEditDTO(Teacher teacher) {
-        return new TeacherEditDTO(teacher.getUuid(), teacher.getFirstname(),
-                teacher.getLastname(), teacher.getVat(), teacher.getRegion().getId());
     }
 }
